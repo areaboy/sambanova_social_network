240028f066b6fcff7836568d878ccd346aeca679
Good Boy <50068759+areaboy@users.noreply.github.com>
2024-11-22 01:57:38 -0800

    Update settings.php

--- sambanova_social_network/backend/settings.php
+++ sambanova_social_network/backend/settings.php
@@ -2,7 +2,7 @@
 error_reporting(0); 
 
 // Sambanova API Key   https://cloud.sambanova.ai/
-$sambanova_api_key ='fcb81a47-33f9-4044-9d16-0373a8c1dbda';
+$sambanova_api_key ='';
 $sambanova_api_model ='Meta-Llama-3.1-8B-Instruct';
 $sambanova_api_url ='https://api.sambanova.ai/v1/chat/completions';
 
@@ -12,7 +12,7 @@ $sambanova_api_url ='https://api.sambanova.ai/v1/chat/completions';
 $logo = "logo.png";
 $title = "Social Network Spark";
 
-$description = "An <b>Online Bully Detection & Social Networking Sites Contents  Analyzer <b> Powered by Sambanova AI...";
+$description = "An Online Bully Detection & Social Networking Sites Contents Analyzer Powered by Sambanova AI...";
 //$description = "Let's Build <b>Online Harassment/Bully Free & easy to analyze Social Networking Sites Contents, Blogs Sites Contents, CRM etc.<b> Powered by Sambanova AI...";
 
 
